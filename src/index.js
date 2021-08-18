180bed79e3119b6703f505ef3c4cb120cc63f72d
kvitdd <dianatrotskaya@gmail.com>
2021-08-18 16:37:57 +0300

    feat: complete task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n > 0) {
+        var n = n.toString().split("");
+        var reversed = Number(n.reverse().join(""));
+    } else if (n < 0) {
+        n = Math.abs(n);
+        var n = n.toString().split("");
+        var reversed = Number(n.reverse().join(""));
+    }
+    return reversed;
+};
